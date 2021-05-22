3048525e8eedf4a1a53285138be03d0865f4d637
zusong <419894552@qq.com>
2021-05-23 00:56:49 +0800

    first commit

---
+++ configurator.go
@@ -0,0 +1,35 @@
+package configurator
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+func Load(config interface{}, files ...string) (err error) {
+	for _, file := range files {
+		if err = loadFile(config, file); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func loadFile(config interface{}, file string) error {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	switch {
+	case strings.HasSuffix(file, ".yaml") || strings.HasSuffix(file, ".yml"):
+		return yaml.Unmarshal(data, config)
+	case strings.HasSuffix(file, ".json"):
+		return json.Unmarshal(data, config)
+	default:
+		return errors.New("supported file type:yaml,json")
+	}
+}
